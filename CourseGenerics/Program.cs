0d8078283f0f91cd7b5d0bb61cc3a95f42d2a46e
Denis Lima <denis.lima1@hotmail.com>
2020-09-28 19:49:02 -0300

    Restricoes de Generics

--- CourseGenerics/Program.cs
+++ CourseGenerics/Program.cs
@@ -1,11 +1,44 @@
 ﻿using Course;
+using Course.Entities;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace CourseGenerics
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            CalculationService();
+
+            Console.ReadLine();
+        }
+
+        static void CalculationService() 
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.Write("Max: ");
+            Console.WriteLine(max);
+        }
+
+        static void PrintService()
         {
             PrintService<int> printService = new PrintService<int>();
 
