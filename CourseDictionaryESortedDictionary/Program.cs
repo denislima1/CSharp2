16b9a93e69c92dedb75225dff5700b8339c50d58
Denis Lima <denis.lima1@hotmail.com>
2020-09-28 22:01:50 -0300

    Dictionary E SortedDictionary

---
+++ CourseDictionaryESortedDictionary/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseDictionaryESortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void dictionaryESortedDictionary()
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99771122";
+            cookies["phone"] = "99771133";
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email");
+            Console.WriteLine("Phone number: " + cookies["phone"]);
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine("Email: " + cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is not 'email' key");
+            }
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.WriteLine("ALL COOKIES:");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
