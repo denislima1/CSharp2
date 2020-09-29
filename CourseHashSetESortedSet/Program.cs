6e5911f3377c439a201671260d96d1077ac1564e
Denis Lima <denis.lima1@hotmail.com>
2020-09-28 21:22:29 -0300

    HeadSet SortedSet

---
+++ CourseHashSetESortedSet/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseHashSetESortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hashSet();
+
+            sortedSet();
+
+            Console.ReadLine();
+        }
+
+        static void hashSet()
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        static void sortedSet()
+        {
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            // union
+
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            //intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            //difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+
+
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
