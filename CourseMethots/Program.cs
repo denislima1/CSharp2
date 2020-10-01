214c54fdbce68a25ff55f3c41ca98e3777cde4fe
Denis Lima <denis.lima1@hotmail.com>
2020-10-01 18:05:26 -0300

    Methods

---
+++ CourseMethots/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CourseMethots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+            Console.ReadLine();
+        }
+
+        static void st()
+        {
+                                      //ano,mes,dia | horario
+            DateTime dt = new DateTime(2020, 10, 01, 17, 53, 00);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
