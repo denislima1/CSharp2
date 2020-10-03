74b32e135e9db1bcd35207784cbdb805579e83f8
Denis Lima <denis.lima1@hotmail.com>
2020-10-03 13:02:12 -0300

    Func

--- CoursePredicate/Program.cs
+++ CoursePredicate/Program.cs
@@ -2,14 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CoursePredicate
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            FuncExample();
+
+            Console.ReadLine();
+        }
+
+        public static void FuncExample()
         {
             List<Product> list = new List<Product>();
 
@@ -18,7 +23,31 @@ namespace CoursePredicate
             list.Add(new Product("Tablet", 350.05));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = p => { p.Price += p.Price * 0.1; };
+            //Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
+
+            List<string> result =  list.Select(func).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        //static string NameUpper(Product p)
+        //{
+        //    return p.Name.ToUpper();
+        //}
+
+        public static void ActionExample()
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.05));
+            list.Add(new Product("HD Case", 80.90));
+
+            //Action<Product> act = p => { p.Price += p.Price * 0.1; };
 
             list.ForEach(p => { p.Price += p.Price * 0.1; });
             //list.ForEach(act);
@@ -26,9 +55,8 @@ namespace CoursePredicate
             {
                 Console.WriteLine(p);
             }
-
-            Console.ReadLine();
         }
+
 
         static void UpdatePrice(Product p)
         {
